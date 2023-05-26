959d92a39eecdf64f58abb91fc9db27eec09edb9
kari212121 <128652282+kari212121@users.noreply.github.com>
2023-05-25 22:16:25 -0600

    Add files via upload

---
+++ blogfundaciones.js
@@ -0,0 +1,31 @@
+function iniciar(){
+    var boton=document.getElementById('guardar');
+    boton.addEventListener('click', nuevoitem, false);
+    mostrar();
+    }
+    function nuevoitem(){
+    var clave=document.getElementById('titulo').value;
+    var valor=document.getElementById('contenido').value;
+    localStorage.setItem(clave,valor);
+    mostrar();
+    document.getElementById('titulo').value='';
+    document.getElementById('contenido').value='';
+    }
+    function mostrar(){
+    var cajadatos=document.getElementById('cajadatos');
+    cajadatos.innerHTML='';
+    for(var f=0;f<localStorage.length;f++){
+    var clave=localStorage.key(f);
+    var valor=localStorage.getItem(clave);
+    cajadatos.innerHTML+='<div>'+clave+' - '+valor+'</div>';
+    }
+    }
+    window.addEventListener('load', iniciar, false);
+    function elimina()
+    {
+    
+        if(confirm('Está Seguro?')){
+            localStorage.clear();
+            mostrar();
+            }
+    }
